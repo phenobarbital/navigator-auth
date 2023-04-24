9fdc2946e0e8092db94672c7bba50ca5bf331d14
Jesus Lara <jesuslarag@gmail.com>
2023-04-24 04:51:56 +0200

    fix bug when a resource match with another (too wide regex)

--- navigator_auth/abac/policies/abstract.py
+++ navigator_auth/abac/policies/abstract.py
@@ -26,14 +26,18 @@ class PolicyResponse(ClassDict):
 
 class Exp:
     def __init__(self, value):
+        self._negative = False
         try:
             self.resource_type, self.raw_value = value.split(':', 1)
         except (ValueError, TypeError):
             self.resource_type = 'uri'
             self.raw_value = value
+        if self.raw_value.startswith('!'):
+            self._negative = True
+            self.raw_value = self.raw_value[1:]
         self.is_regex = False
         try:
-            self.value = re.compile(self.raw_value)
+            self.value = re.compile(f'^{self.raw_value}$')
             self.is_regex = True
         except re.error:
             logging.warning(f'Resource {value} is not a Regular Expression.')
@@ -45,6 +49,9 @@ class Exp:
 
     def __repr__(self) -> str:
         return f"<{type(self).__name__}({self.resource_type}: {self.value!r})>"
+
+    def is_negative(self):
+        return self._negative
 
     def match(self, value):
         if self.is_regex:
