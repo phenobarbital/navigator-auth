2c908df08efa156b4ce13f432c2d56e5ec195d5d
Jesus Lara <jesuslarag@gmail.com>
2026-02-01 02:14:42 +0100

    fix version of xmlsec vs lxml

--- navigator_auth/version.py
+++ navigator_auth/version.py
@@ -7,7 +7,7 @@ __title__ = "navigator_auth"
 __description__ = (
     "Navigator Auth is an Authentication/Authorization Toolkit for aiohttp."
 )
-__version__ = "0.17.1"  # pragma: no cover
+__version__ = "0.17.2"  # pragma: no cover
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslarag@gmail.com"
 __copyright__ = "Copyright (c) 2019-2025 Jesus Lara"
